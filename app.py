d266575d4f0c26c757242479fba1b6c9437e4b8f
atena <gaishingu@gmail.com>
2026-03-31 14:37:11 +0900

    Update app.run to use environment PORT variable

--- app.py
+++ app.py
@@ -704,4 +704,5 @@ def health_check():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
